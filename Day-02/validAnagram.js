dc01c5f29fc23855cc40bc9a141fcadf0fbf3603
Adityaraval13 <adityaraval1306@gmail.com>
2026-02-27 19:33:40 +0530

    Day 2

---
+++ Day-02/validAnagram.js
@@ -0,0 +1,13 @@
+//An anagram is when two strings contain the same characters in any order.
+
+/*Frequency map : A frequency map is just a plain object {} in js that keeps the track
+ of how many times something occurs.
+ 
+ *It is useful for :
+ - Anagram detection 
+ - Counting characters / digits
+ - Finding duplicates
+ - Frequency based sorting 
+ - Advanced pattern matching+
+ */
+

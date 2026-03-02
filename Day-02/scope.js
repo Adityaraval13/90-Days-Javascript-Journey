bcf1753261a76d929000423582325b944d70e298
Adityaraval13 <adityaraval1306@gmail.com>
2026-03-02 19:16:59 +0530

    Day 3: Solved two sum using Hash map

--- Day-02/scope.js
+++ Day-02/scope.js
@@ -7,3 +7,13 @@ function startEngine() {
 
 startEngine();
 console.log("Outside the engine, fuel is:", fuel);
+
+
+/* const (Your Default): Use this for everything. If the value doesn't need to change 
+(like a function, a Map, or a fixed configuration), use const. It makes your code "read-only" and safer.
+
+ let (The Iterator): Use this only when you know the value will change, 
+ like a counter in a for loop or a toggle.
+
+ var (The Legacy): Avoid this entirely in modern code. 
+ It’s the "Old Way" and leads to the scope bugs we discussed earlier.*/
---
+++ Day-03/twoSum.js
@@ -0,0 +1,55 @@
+/*Given an array of integers nums and an integer target,
+ return the indices i and j such that nums[i] + nums[j] == target and i != j.*/
+
+//Nested loop approach (O(n^2))
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+// let nums = [4,5,6], target = 10
+// console.log(twoSum(nums, target));
+
+
+
+//Hash map approach (O(n))
+
+function twoSumHash(nums ,target){
+// 1. Create a new Map to act as our "Memory"
+    const memory = new Map();
+
+    for(let i = 0 ; i < nums.length ; i++){
+
+      
+        const currentNum= nums[i];
+  // 2. Calculate what number we NEED to find
+        const complement = target - currentNum ;
+
+        // 3. Check if the "memory" already has that complement
+        if(memory.has(complement)){
+            // SUCCESS! Return the index of the complement and the current index
+            return [memory.get(complement) ,i]
+        }
+        // 4. If not found, store the current number as the KEY 
+        // and its index as the VALUE
+        memory.set(currentNum, i);
+    }
+}
+
+let nums = [4,5,6], target = 10
+console.log(twoSumHash(nums, target));
+
+
+
+let hero = " super " ; 
+
+function hel(){
+    console.log(hero)
+}
+
+hel();
